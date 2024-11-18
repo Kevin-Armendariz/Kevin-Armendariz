0756371d37f23750ed66c54af4945b5856464d02
Kevin-Armendariz <22308051220544@cbtis122.edu.mx>
2024-11-17 20:43:49 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,36 @@ import ItemActividad from './itemsactividades/ItemActividad';
 import datos from './data/jiji.json';
 
 function App() {
-   const Datosvacios = {
-    id: "", 
-    nombre: "",
-    descripcion: "", 
-    estado: false, 
-    fecha: ""
-  };
- 
-  const [data, setData] = useState(datos); 
-  const [itemData, setItemData] = useState(Datosvacios);
+  const blankData = {
+    "id":"", 
+    "nombre":"",
+    "descripcion":"", 
+    "estado":false, 
+    "fecha":""};
 
-  const handleClicAdd = (value) => {
-    setData(value);
-  };
+    const[data, setData] = useState(Datos);
+    const [itemData, setItemData] = useState(blankData);
 
-  const handleClickUpdate = (value) => {
-    const dataUpdate = data.map((item) => {
-      if (item.id === value.id) {
-        return { ...item, ...value };
-      }
-      return item;
-    });
-    setItemData(value);
-    setData(dataUpdate);
-  };
-
+    const handleClicAdd = (value) =>{
+      setData(value);
+    };
+    const handleClicDetail = (value) =>{
+      setItemData(value);
+    };
+    const handleClickUpdate = (value) =>{
+      const dataUpdate = data.map((item)=>{
+          if (item.id == value.id) {
+            return { ...item, ...value };
+          }
+          return item;
+      })
+      setItemData(value);
+      setData(dataUpdate);
+    };
+    const handleDeleteActivity = (id) => {
+      setData((prevData) => prevData.filter((item) => item.id !== id));
+      setItemData(blankData);
+    };
   return (
     <div className='principal'>
       <header className='header'>To Do List</header>
